1d1c6df574661d039bcbc1777673b34c2eea6be3
SPFlores <sflores92886@gmail.com>
2019-03-01 18:53:27 -0800

    psuedo coding JS file

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// only allow letters to be pressed not numbers
+
+// make an array (object?) of words computer can choose from
+
+// compare user input to letters in word, fill in if there, decrease number of guesses remaining if not there
+
+// if word completed before guesses = 0, show corresponding image, increase wins by one; if not completed, show fail image and reset guesses, word, and increase losses by one
+
+// make sure you can't input the same letter more than once
