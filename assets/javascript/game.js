7e815fd992b84226fe1c96c1321ae0082e5a5df9
SPFlores <sflores92886@gmail.com>
2019-03-01 20:05:10 -0800

    choices

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 // only allow letters to be pressed, not numbers
 
 // make an array (object?) of words computer can choose from
+const computerChoices = [ `Sam Winchester`, `Dean Winchester`, `Crowley`, `Castiel`, `pie`, `angels and demons`, `devil's trap`, `leviathan`, `Impala`, `Enochian` ]
 
 // compare user input to letters in word, fill in if there, decrease number of guesses remaining if not there
 
