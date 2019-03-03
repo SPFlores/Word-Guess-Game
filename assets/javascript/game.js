09b13b0d01d365692809701af41f896c78b9a7aa
SPFlores <sflores92886@gmail.com>
2019-03-03 11:37:16 -0800

    entering letters into word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,90 @@
-console.log(computerGuess)
-
 const noNumbersHere = () => {
   document.onkeyup = (event) => {
     if (event.keyCode <= 57 && event.keyCode >= 48) {
       alert(`You can only choose letters in this game!`)
     } else {
-      //  run function to determine if the letter is part of the word
+
     }
   }
 }
 
-const computerChoices = [`winchester`, `dean`, `sam`, `crowley`, `castiel`, `pie`, `angels`, `demons`, `leviathan`, `impala`, `enochian`]
-var remainingLetters = computerGuess.length
-const computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+const computerChoices = [`sam-winchester`, `dean-winchester`, `crowley`, `castiel`, `pie`, `angels`, `demons`, `leviathan`, `impala`, `enochian`]
+const computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+// var remainingLetters = computerGuess.length
 let wins = 0
 let losses = 0
 let guessesLeft = 12
-let guesses = [] 
-const answerArray = []
-for (let i = 0; i < computerGuess.length; i++) {
-  answerArray[i] = '_'
+let guesses = []
+var showWins = document.getElementById("mywins")
+var showLosses = document.getElementById("myloses")
+
+console.log(computerChoice)
+
+let result = () => {
+  wordHolder = document.getElementById('hold')
+  correct = document.createElement('ul')
+
+  for (var i = 0; i < computerChoice.length; i++) {
+    correct.setAttribute('id', 'my-word')
+    guess = document.createElement('li')
+    guess.setAttribute('class', 'guess')
+    if (computerChoice[i] === '-') {
+      guess.innerHTML = ' '
+      space = 1
+    } else {
+      guess.innerHTML = '_'
+    }
+
+    guesses.push(guess)
+    wordHolder.appendChild(correct)
+    correct.appendChild(guess)
+  }
 }
+
+check = function () {
+  document.onkeyup = (event) => {
+    if (event.keyCode <= 57 && event.keyCode >= 48) {
+      document.querySelector('#warning').innerHTML = `You can only choose letters in this game`
+    } else {
+      document.querySelector('#warning').innerHTML = ' '
+      let guess = (event.key)
+      for (var i = 0; i < computerChoice.length; i++) {
+        if (computerChoice[i] === guess) {
+          guesses[i].innerHTML = guess
+          counter += 1
+        }
+      }
+      // var j = (computerChoice.indexOf(guess))
+      // if (j === -1) {
+      //   lives -= 1
+      //   comments()  // lose 
+      //   animate()  // change picture
+      // } else {
+      //   comments()
+      // }
+    }
+  }
+}
+
+check()
+result()
+
 // to change wins and losses
+
+// compare user input to letters in word, fill in if there, decrease number of guesses remaining if not there
+
+// if word completed before guesses = 0, show corresponding image, increase wins by one, pick another word; if not completed, show fail image and reset guesses, word, and increase losses by one
+
 progress = function () {
   showWins.innerHTML = `You have won ${wins = 0} games`
   showLosses.innerHTML = `You have lost ${losses = 0} games`
   for (var i = 0; i < geusses.length; i++) {
     if (computerGuess.indexOf('_') === -1) {
-      show.Losses.innerHTML
+      showWins.innerHTML
+    } else {
 
     }
   }
 }
-
-// compare user input to letters in word, fill in if there, decrease number of guesses remaining if not there
-
-// if word completed before guesses = 0, show corresponding image, increase wins by one, pick another word; if not completed, show fail image and reset guesses, word, and increase losses by one
 
 // make sure you can't input the same letter more than once
