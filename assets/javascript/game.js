98a800818ea844d77b866d18611b32ff67214dca
SPFlores <sflores92886@gmail.com>
2019-03-02 19:04:24 -0800

    rearranging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
-// only allow letters to be pressed, not numbers
+console.log(computerGuess)
+
 const noNumbersHere = () => {
   document.onkeyup = (event) => {
     if (event.keyCode <= 57 && event.keyCode >= 48) {
@@ -9,42 +10,30 @@ const noNumbersHere = () => {
   }
 }
 
-// make an array (object?) of words computer can choose from
-const computerChoices = [`Sam Winchester`, `Dean Winchester`, `Crowley`, `Castiel`, `pie`, `angels and demons`, `devils trap`, `leviathan`, `Impala`, `Enochian`]
-
+const computerChoices = [`winchester`, `dean`, `sam`, `crowley`, `castiel`, `pie`, `angels`, `demons`, `leviathan`, `impala`, `enochian`]
+var remainingLetters = computerGuess.length
 const computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
-
-console.log(computerGuess)
-
 let wins = 0
 let losses = 0
 let guessesLeft = 12
 let guesses = [] 
+const answerArray = []
+for (let i = 0; i < computerGuess.length; i++) {
+  answerArray[i] = '_'
+}
+// to change wins and losses
+progress = function () {
+  showWins.innerHTML = `You have won ${wins = 0} games`
+  showLosses.innerHTML = `You have lost ${losses = 0} games`
+  for (var i = 0; i < geusses.length; i++) {
+    if (computerGuess.indexOf('_') === -1) {
+      show.Losses.innerHTML
+
+    }
+  }
+}
 
 // compare user input to letters in word, fill in if there, decrease number of guesses remaining if not there
-
-document.onkeyup = e => {
-  noNumbersHere()
-  // if (computerChoices.indexOf(e.key) !== -1) {
-  //   if (e.key === computerGuess) {
-  //     ties++
-  //   } else {
-  //     switch (e.key) {
-  //       case 'r':
-  //         computerGuess === 'p' ? losses++ : wins++
-  //         break
-  //       case 'p':
-  //         computerGuess === 's' ? losses++ : wins++
-  //         break
-  //       case 's':
-  //         computerGuess === 'r' ? losses++ : wins++
-  //         break
-  //       default:
-  //         break
-  //     }
-  //   }
-  // }
-}
 
 // if word completed before guesses = 0, show corresponding image, increase wins by one, pick another word; if not completed, show fail image and reset guesses, word, and increase losses by one
 
